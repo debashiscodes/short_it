32240d55b0ab09103d74bc288080359e923bfc81
Debashis Biswal <debashiscodes@gmail.com>
2023-05-25 13:55:44 +0530

    Add shorten link algo

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -3,5 +3,17 @@ class UrlsController < ApplicationController
   end
 
   def create
+    @url = Url.new(url_params)
+    if @url.save
+      redirect_to @url
+    else
+      render "index"
+    end
+  end
+
+  private
+
+  def url_params
+    params.require(:url).permit(:long_url)
   end
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,20 @@
 class Url < ApplicationRecord
+  validates :long_url, :short_url, presence: true
+  validates :short_url, uniqueness: true
+  validates_format_of :long_url, :with => /\A(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?\Z/i
+
+  before_validation :set_short_url
+
+  private
+
+  def set_short_url
+    self.short_url = "http://localhost:3000/" + generate_short_url
+  end
+
+  def generate_short_url
+    loop do
+      short_url = SecureRandom.uuid[0..6]
+      break short_url unless Url.exists?(short_url: short_url)
+    end
+  end
 end
