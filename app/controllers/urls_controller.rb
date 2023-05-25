d8a2c3998ed032795b54070c87ad0ac73333a940
Debashis Biswal <debashiscodes@gmail.com>
2023-05-25 14:06:43 +0530

    Add routes for shw

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -2,10 +2,14 @@ class UrlsController < ApplicationController
   def index
   end
 
+  def show
+    @url = Url.find(params[:id])
+  end
+
   def create
     @url = Url.new(url_params)
     if @url.save
-      redirect_to @url
+      redirect_to show_routes_path(@url.id)
     else
       render "index"
     end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'urls#index'
 
   post '/urls' => 'urls#create'
+  get '/urls/:id' => 'urls#show',  :as => 'show_routes'
 end
